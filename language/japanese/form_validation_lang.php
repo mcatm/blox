62edea9660e2bbeb231d4cdd52139147431e1ca4
mcatm <tkcs@pelepop.com>
2010-06-26 16:37:45 +0900

    languages (Japanese)

---
+++ language/japanese/form_validation_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['required'] 			= "%sが未入力です";
+$lang['isset']				= "%sには、何か入力する必要があります";
+$lang['valid_email']		= "%sは正しいE-Mailアドレスではありません";
+$lang['valid_emails'] 		= "%sに、不正なE-Mailアドレスがあります";
+$lang['valid_url'] 			= "%sは正しいURLを入力する必要があります";
+$lang['valid_ip'] 			= "The %s field must contain a valid IP.";
+$lang['min_length']			= "The %s field must be at least %s characters in length.";
+$lang['max_length']			= "The %s field can not exceed %s characters in length.";
+$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
+$lang['alpha']				= "The %s field may only contain alphabetical characters.";
+$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
+$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['numeric']			= "The %s field must contain a number.";
+$lang['is_numeric']			= "The %s field must contain a number.";
+$lang['integer']			= "The %s field must contain an integer.";
+$lang['matches']			= "The %s field does not match the %s field.";
+$lang['is_natural']			= "The %s field must contain a number.";
+$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
+
+$lang['check_email']			= "このE-Mailアドレスは既に使用されています";
+$lang['check_account']			= "このアカウントは既に使用されています";
+
+/* End of file validation_lang.php */
+/* Location: ./system/language/english/validation_lang.php */
