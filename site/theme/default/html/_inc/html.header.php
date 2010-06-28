29fdb45bc67bb7c676dfd586940032038e76d744
mcatm <tkcs@pelepop.com>
2010-06-28 11:23:34 +0900

    theme (default)

---
+++ site/theme/default/html/_inc/html.header.php
@@ -0,0 +1,6 @@
+<?if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?=$setting['title']?></title>
+</head>
---
+++ site/theme/default/html/post.detail.php
@@ -0,0 +1,24 @@
+<?if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?$this->load->view('_inc/html.header.php')?>
+<body>
+	<?$this->load->view('_inc/head.php')?>
+	<?if(isset($post)){?><div>
+		<?if(isset($post[0]['file_main'])){?><p><?foreach($post[0]['file_main'] as $fm){?><?if($fm['type'] == 'image'){?><img src="<?=img_url($fm['id'], 200)?>" /><?}}?></p><?}?>
+		<h3><a href="<?=$post[0]['url']?>"><?=$post[0]['title']?></a></h3>
+		<p><?=format_text($post[0]['text'])?></p>
+		<p><?=$post[0]['createdate']?></p>
+		<p><?=$post[0]['alias']?></p>
+		<p><?=$post[0]['author'][0]['name']?></p>
+		<?if(isset($post['tag'])){?><p><?foreach($post[0]['tag'] as $t){?><?=$t['name']?> / <?}?></p><?}?>
+		<hr />
+		<h4>access</h4>
+		<p>count: <?=$access[0]['value']['count']['total']?> (一日あたり<?=$access[0]['value']['count']['rate']?>）</p>
+		<p>last access: <?=$access[0]['update']?></p>
+		
+	</div><?} else {?>このエントリは削除されたか、閲覧出来ない設定になっております<?}?>
+</body>
+</html>
+
+<hr />
+
+<p><?print_r($post)?></p>
