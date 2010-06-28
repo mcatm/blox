37a0277c08eb999d16b9ce9e4d4909fce4dfe4a5
mcatm <tkcs@pelepop.com>
2010-06-28 11:22:50 +0900

    language

---
+++ site/language/english/system_lang.php
@@ -0,0 +1,56 @@
+<?php
+
+$lang['system_label_post'] = 'post';
+$lang['system_label_post_list'] = 'list';
+$lang['system_label_post_new'] = 'new';
+$lang['system_label_comment'] = 'comment';
+$lang['system_label_tag'] = 'tag';
+$lang['system_label_site'] = 'site';
+$lang['system_label_theme'] = 'theme';
+$lang['system_label_div'] = 'div';
+$lang['system_label_setting'] = 'setting';
+$lang['system_label_install'] = 'install';
+$lang['system_label_user'] = 'user';
+$lang['system_label_user_list'] = 'list';
+$lang['system_label_user_new'] = 'new';
+$lang['system_label_user_invite'] = 'invite';
+$lang['system_label_usertype'] = 'usertype';
+$lang['system_label_file'] = 'file';
+$lang['system_label_file_upload'] = 'upload';
+$lang['system_label_file_list'] = 'list';
+$lang['system_label_file_image'] = 'image';
+$lang['system_label_file_sound'] = 'sound';
+$lang['system_label_analytic'] = 'analytics';
+
+$lang['system_post_add'] = 'add';
+$lang['system_post_edit'] = 'edit';
+$lang['system_post_delete'] = 'delete';
+$lang['system_post_preview'] = 'preview';
+$lang['system_post_detail'] = 'detail';
+
+$lang['system_post_success'] = "you posted it successfully.";
+$lang['system_post_error'] = "an error occured in your posting.";
+$lang['system_post_error_auth'] = "an error occured because you don't have an authority.";
+
+$lang['system_post_label_title'] = 'title';
+$lang['system_post_label_text'] = 'text';
+$lang['system_post_label_status'] = 'status';
+$lang['system_post_label_file'] = 'file';
+$lang['system_post_label_div'] = 'section/category';
+$lang['system_post_label_author'] = 'author';
+$lang['system_post_label_number'] = 'number';
+$lang['system_post_label_parent'] = 'parent post';
+$lang['system_post_label_type'] = 'post type';
+$lang['system_post_label_createdate'] = 'post date';
+$lang['system_post_label_tag'] = 'tag';
+$lang['system_post_label_alias'] = 'alias for permalink';
+
+$lang['system_user_label_name'] = 'name';
+$lang['system_user_label_account'] = 'account';
+$lang['system_user_label_description'] = 'description';
+$lang['system_user_label_title'] = 'title';
+$lang['system_user_label_password'] = 'password';
+
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/english/calendar_lang.php */
---
+++ site/language/japanese/system_lang.php
@@ -0,0 +1,83 @@
+<?php
+
+$lang['system_label_post']			= '記事';
+$lang['system_label_post_list']		= '記事一覧';
+$lang['system_label_post_new']		= '記事追加';
+$lang['system_label_comment']		= 'コメント';
+$lang['system_label_tag']			= 'タグ';
+$lang['system_label_site']			= 'サイト設定';
+$lang['system_label_theme']			= 'テーマ';
+$lang['system_label_div']			= '分類';
+$lang['system_label_mail']			= 'メール';
+$lang['system_label_setting']		= '設定変更';
+$lang['system_label_install']		= 'インストール';
+$lang['system_label_user']			= 'ユーザー';
+$lang['system_label_user_list']		= '会員一覧';
+$lang['system_label_user_new']		= '会員追加';
+$lang['system_label_user_invite']	= '招待';
+$lang['system_label_usertype']		= 'ユーザータイプ';
+$lang['system_label_file']			= 'ファイル';
+$lang['system_label_file_upload']	= 'アップロード';
+$lang['system_label_file_list']		= 'ファイル一覧';
+$lang['system_label_file_image']	= '画像一覧';
+$lang['system_label_file_sound']	= '音声ファイル一覧';
+$lang['system_label_analytic']		= '解析';
+
+$lang['system_post_add'] = '記事投稿';
+$lang['system_post_edit'] = '記事編集';
+$lang['system_post_delete'] = '削除';
+$lang['system_post_preview'] = 'preview';
+$lang['system_post_detail'] = 'detail';
+
+$lang['system_user_edit'] = '会員情報編集';
+$lang['system_user_add'] = '会員情報追加';
+$lang['system_user_invite'] = '招待';
+
+$lang['system_file_list'] = 'ファイル一覧';
+$lang['system_file_add'] = 'ファイル追加';
+
+$lang['system_div_list'] = 'ページ一覧';
+$lang['system_div_edit'] = 'ページ編集';
+$lang['system_div_add'] = 'ページ追加';
+
+$lang['system_mail_list'] = 'メール一覧';
+
+$lang['system_post_success'] = "正常に投稿が完了しました";
+$lang['system_post_error'] = "エラーが発生しました";
+$lang['system_post_error_auth'] = "投稿権限がありません";
+
+$lang['system_post_status_0'] = "公開";
+$lang['system_post_status_1'] = "指定日投稿";
+$lang['system_post_status_9'] = "下書き";
+
+$lang['system_post_label_title'] = 'title';
+$lang['system_post_label_text'] = 'text';
+$lang['system_post_label_status'] = 'status';
+$lang['system_post_label_file'] = 'file';
+$lang['system_post_label_div'] = 'section/category';
+$lang['system_post_label_author'] = 'author';
+$lang['system_post_label_number'] = 'number';
+$lang['system_post_label_parent'] = 'parent post';
+$lang['system_post_label_type'] = 'post type';
+$lang['system_post_label_createdate'] = 'post date';
+$lang['system_post_label_tag'] = 'tag';
+$lang['system_post_label_alias'] = 'alias for permalink';
+
+$lang['system_user_label_name'] = 'name';
+$lang['system_user_label_account'] = 'account';
+$lang['system_user_label_email'] = 'E-Mail';
+$lang['system_user_label_description'] = 'description';
+$lang['system_user_label_title'] = 'title';
+$lang['system_user_label_password'] = 'password';
+$lang['system_user_label_password_confirm'] = '確認用パスワード';
+
+$lang['system_email_title_send'] = 'メールを送信しました';
+$lang['system_email_title_receive']	= 'メールを受信しました';
+
+$lang['mail_content_address_0_postal']	= '郵便番号';
+$lang['mail_content_tel']	= '電話番号';
+$lang['system_mail_label_require']	= '必須項目';
+
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/english/calendar_lang.php */
