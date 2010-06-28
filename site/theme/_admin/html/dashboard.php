e0161914285b37f65a14d92a2e23a4bb0108acc7
mcatm <tkcs@pelepop.com>
2010-06-28 16:07:33 +0900

    update admins theme.

--- site/theme/_admin/html/dashboard.php
+++ site/theme/_admin/html/dashboard.php
@@ -4,37 +4,55 @@
 	<?$this->load->view('_inc/head.php')?>
 	<div id="body">
 		<div class="container">
-			<?if(isset($post)){?>
-			<h2><a href="<?=base_url()?>admin/post/">記事</a></h2>
-			<ul>
-				<?foreach($post as $k => $v){?><li>
-					<h3><a href="<?=$v['admin_url']?>"><?=$v['title']?></a></h3>
-					<p><?=$v['text']?></p>
-					<p><?=$v['alias']?></p>
-				</li><?}?>
-			</ul>
-			<hr /><?}?>
-			
-			<?if(isset($comment)){?>
-			<h2><a href="<?=base_url()?>admin/comment/">コメント</a></h2>
-			<ul>
-				<?foreach($comment as $k => $v){?><li>
-					<h3><a href="<?=$v['admin_url']?>"><?=$v['title']?></a></h3>
-					<p><?=$v['text']?></p>
-					<p><?=$v['alias']?></p>
-				</li><?}?>
-			</ul>
-			<hr /><?}?>
-			
-			<?if(isset($user)){?>
-			<h2><a href="<?=base_url()?>admin/user/">ユーザー</a></h2>
-			<ul>
-				<?foreach($user as $k => $v){?><li>
-					<h3><?=$v['name']?></h3>
-					<p><?=$v['description']?></p>
-				</li><?}?>
-			</ul>
-			<hr /><?}?>
+			<div class="dashboard clearfix">
+				<?if(isset($post)){?><div class="block">
+					<div class="title">
+						<h2><a href="<?=base_url()?>admin/post/">記事</a></h2>
+					</div>
+					<ul>
+						<?foreach($post as $k => $v){?><li>
+							<h3><a href="<?=$v['admin_url']?>"><?=$v['title']?></a></h3>
+							<p><?=format_description($v['text'], 140)?></p>
+						</li><?}?>
+					</ul>
+				</div><?}?>
+				
+				<?if(isset($comment)){?><div class="block">
+					<div class="title">
+						<h2><a href="<?=base_url()?>admin/comment/">コメント</a></h2>
+					</div>
+					<ul>
+						<?foreach($comment as $k => $v){?><li>
+							<h3><?=$v['title']?></h3>
+							<p><?=format_description($v['text'], 140)?></p>
+						</li><?}?>
+					</ul>
+				</div><?}?>
+				
+				<?if(isset($user)){?><div class="block">
+					<div class="title">
+						<h2><a href="<?=base_url()?>admin/user/">ユーザー</a></h2>
+					</div>
+					<ul>
+						<?foreach($user as $k => $v){?><li>
+							<h3><?=$v['name']?></h3>
+							<p><?=format_description($v['description'], 100)?></p>
+						</li><?}?>
+					</ul>
+				</div><?}?>
+				
+				<?if(isset($weekly)){?><div class="block">
+					<div class="title">
+						<h2><a href="<?=base_url()?>admin/user/">スケジュール</a></h2>
+					</div>
+					<ul>
+						<?foreach($weekly as $k => $v){?><li>
+							<h3><?=$v['title']?></h3>
+							<p><?=format_description($v['text'], 140)?></p>
+						</li><?}?>
+					</ul>
+				</div><?}?>
+			</div>
 		</div>
 	</div>
 	<?$this->load->view('_inc/foot.php')?>
