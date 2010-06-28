a87f81e288f0cf8de1b32e2b5e7846b0e4e5a99c
mcatm <tkcs@pelepop.com>
2010-06-28 18:17:34 +0900

    Change Files.

--- site/theme/_admin/html/_inc/head.php
+++ site/theme/_admin/html/_inc/head.php
@@ -14,18 +14,22 @@
 			<div class="logout"><a href="<?=base_url()?>admin/logout/">logout</a></div>
 		</div>
 	</div>
-	<div class="window">
+	<div class="window clearfix">
 		<div class="search clearfix" id="tool-search">
 			<div class="result" id="tool-search-result"></div>
 			<div class="navi round" id="tool-search-navi">
 				<input type="text" class="round" id="tool-search-query" />
 			</div>
 		</div>
-		<div class="profile clearfix" id="tool-profile">
-			<?if(isset($me['file_main'])){?><a href="<?=base_url()?>admin/user/edit/<?=$me['id']?>/"><img src="<?=img_url($me['file_main'][0]['id'], 48, true)?>" /></a><?}?>
-			<h2><a href="<?=base_url()?>admin/user/edit/<?=$me['id']?>/"><?=$me['name']?></a></h2>
-			<p class="auth"><?=$me['auth']['name']?></p>
-			<p class="account"><?=$me['account']?></p>
+		<div class="profile" id="tool-profile">
+			<div class="container clearfix">
+				<h2><a href="<?=base_url()?>admin/user/edit/<?=$me['id']?>/"><?=$me['name']?></a></h2>
+				<?if(isset($me['file_main'])){?><div class="avater"><a href="<?=base_url()?>admin/user/edit/<?=$me['id']?>/"><img src="<?=img_url($me['file_main'][0]['id'], 48, true)?>" /></a></div><?}?>
+				<div class="data">
+					<p class="auth"><?=$me['auth']['name']?></p>
+					<p class="account"><?=$me['account']?></p>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
--- site/theme/default/html/_inc/html.header.php
+++ site/theme/default/html/_inc/html.header.php
@@ -1,6 +1,18 @@
-<?if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?=$setting['title']?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta name="Description" content="<?=$this->setting->get('description')?>" />
+<meta name="Keywords" content="<?=$this->setting->get('keyword')?>" />
+
+<title><?=$this->setting->get('title')?></title>
+
+<link rel="stylesheet" href="<?=theme_url()?>css/import.css" type="text/css" />
+
+<script type="text/javascript" src="<?=ex_url()?>js/jquery.js"></script>
+<script type="text/javascript" src="<?=ex_url()?>js/jquery/smoothscroll.js"></script>
+
+<link href="<?=base_url()?>ex/favicon.ico" rel="shortcut icon" />
+
 </head>
