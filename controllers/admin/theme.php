6652d456f5cf2033b2d9a3cbde3739453b372b99
mcatm <tkcs@pelepop.com>
2010-08-17 19:43:38 +0900

    Dev : theme editor

--- controllers/admin/theme.php
+++ controllers/admin/theme.php
@@ -1,20 +1,41 @@
 <?php
 
 class Theme extends Controller {
-
+	
+	var $out = array();
+	
 	function index($dir = "") {
 		$this->load->helper('directory');
 		$tpl = directory_map(THEME_FOLDER);
 		$map = array();
 		
-		if (empty($dir)) {
-			foreach($tpl as $k => $v) {
-				if (is_array($v) && !preg_match('(^_(.*))', $k)) {
-					print '<h1><a href="'.base_url().'admin/theme/'.$k.'/">'.$k.'</a></h1>';
-					print_r($v);
-					print '<hr />';
-				}
+		$base_segment = array();
+		for($i=3;$this->uri->segment($i);$i++) $base_segment[] = $this->uri->segment($i);
+		#print_r($base_segment);exit;
+		
+		$this->data->out['page']['base_segment'] = implode($base_segment, '/');
+		if (!empty($this->data->out['page']['base_segment'])) $this->data->out['page']['base_segment'] .= "/";
+		#exit($this->data->out['page']['base_segment']);
+		
+		$out = $this->_get_tpl($tpl, array());
+		
+		#print_r($out);
+		
+		foreach ($base_segment as $bs) $out = $out[$bs];
+		$this->data->out['tpl'] = $out;
+		#print_r($out);exit;
+		/*if (empty($dir)) {
+			$this->data->out['tpl'] = $out;
+		} else {
+			if (empty($subdir)) {
+				$this->data->out['tpl'] = $out[$dir];
+			} else {
+				$this->data->out['tpl']	= $out[$dir][$subdir];
 			}
+		}*/
+		
+		/*if (empty($dir)) {
+			
 		} else {
 			$path = THEME_FOLDER.'/'.$dir;
 			if (!is_file($path)) {
@@ -22,12 +43,27 @@ class Theme extends Controller {
 				foreach ($p as $tm) $tpl = $tpl[$tm];
 				foreach ($tpl as $tmp_k => $tmp) {
 					$u = (is_array($tmp)) ? $tmp_k : $tmp;
-					print '<a href="'.base_url().'admin/theme/'.$dir.'/'.$u.'">'.$u.'<br />';
+					
+					#print '<a href="'.base_url().'admin/theme/'.$dir.'/'.$u.'">'.$u.'<br />';
 				}
 			} else {
 				print '<textarea style="width:900px;height:500px">'.file_get_contents($path).'</textarea>';
 			}
+		}*/
+		$this->load->view('theme.list.php');
+	}
+	
+	private function _get_tpl($tpl = array(), $out = array(), $is_theme = true) {
+		foreach($tpl as $k => $v) {
+			if (!preg_match('(^_(.*))', $k) || $is_theme === false) {
+				if (is_array($v)) {
+					$out[$k] = $this->_get_tpl($v, $out, false);
+				} else {
+					$out[] = $v;
+				}
+			}
 		}
+		return $out;
 	}
 	
 	function _remap($m) {
---
+++ site/theme/_admin/html/theme.list.php
@@ -0,0 +1,84 @@
+<?if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?$this->load->view('_inc/html.header.php')?>
+<body>
+	<?$this->load->view('_inc/head.php')?>
+	<div id="body">
+		<div class="container">
+			<div class="list">
+				<div class="head round">
+					<h2><a href="<?=base_url()?>admin/theme/"><?=$this->lang->line('system_label_theme')?></a></h2>
+					<div class="add"><a href="<?=base_url()?>admin/theme/new/">+ ADD</a> | <a id="trigger_tool_filter">+ FILTER</a></div>
+					<div class="tool">
+						<div id="target_tool_filter" class="filter <?if(!isset($this->session->userdata['filter']['admin/theme']) || count($this->session->userdata['filter']['admin/theme']) == 0){?> hide<?}?>">
+							<div class="sort">
+								createdate: <a href="<?=base_url()?>admin/theme/filter/sort/createdate/desc/">desc</a> | <a href="<?=base_url()?>admin/theme/filter/sort/createdate/asc/">asc</a> | 
+								status: <a href="<?=base_url()?>admin/theme/filter/where/status/0/"><?=$this->setting->get_status(0)?></a> | <a href="<?=base_url()?>admin/theme/filter/where/status/1/"><?=$this->setting->get_status(1)?></a> | <a href="<?=base_url()?>admin/theme/filter/where/status/9/"><?=$this->setting->get_status(9)?></a>
+							</div>
+							<form action="<?=base_url()?>admin/theme/filter/q/" method="POST"><input type="text" name="q" value="<?if(isset($this->session->userdata['filter']['admin/theme']['query'])){?><?=$this->session->userdata['filter']['admin/theme']['query']?><?}?>" /></form>
+							<a href="<?=base_url()?>admin/theme/filter/clear/">filter clear</a>
+						</div>
+					</div>
+				</div>
+				<?if(isset($tpl)){?><form action="<?=base_url()?>admin/theme/delete/" method="post" id="form_list" name="form_list">
+					<table>
+						<?foreach($tpl as $k => $v){?><tr class="theme_id<?=$k?>">
+							<td><h3><a href="<?=base_url()?>admin/theme/<?=$page['base_segment']?><?=$k?>/"><?=$k?></a></h3></td>
+						</tr>
+						<?foreach($v as $tk => $tv){?><tr class="theme_id<?=$k?>">
+							<?if(is_array($tv)){?><td><a href="<?=base_url()?>admin/theme/<?=$page['base_segment']?><?=$k?>/<?=$tk?>/"><?=$tk?></a></td>
+							<?}else{?><td><?=$tv?></td>
+							<?}?>
+						</tr><?}}?>
+					</table>
+					<div id="list_footer" class="clearfix">
+						<div class="tool">
+							<a id="btn-select-all">select all</a> / 
+							<a id="btn-deselect-all">deselect all</a> / 
+							<a id="btn-delete">delete</a>
+						</div>
+						<div class="pager"></div>
+					</div>
+				</form><?}?>
+			</div>
+		</div>
+	</div>
+	<?$this->load->view('_inc/foot.php')?>
+</body>
+<script type="text/javascript">
+	$(function() {
+		$('td.tool .delete').each(function() {
+			$(this).click(function() {
+				post_id = $(this).attr('attr');
+				flg_del = confirm ('削除してもよろしいですか？');
+				if (flg_del) {
+					$.ajax({
+						url: '<?=base_url()?>request/delete/post/',
+						type: 'post',
+						data: {
+							id: post_id
+						}
+					});
+					$('.post_id' + post_id).fadeOut('slow');
+				}
+			});
+		});
+		
+		$('#btn-delete').click(function() {
+			if($('#form_list :checked').length > 0) {
+				flg_del = confirm ('記事を削除します。よろしいですか？');
+				if (flg_del) {
+					document.form_list.submit();
+				}
+			}
+		});
+		
+		$('#btn-select-all').click(function() {
+			$(':checkbox').attr('checked', 'checked');
+		});
+		
+		$('#btn-deselect-all').click(function() {
+			$(':checkbox').removeAttr('checked');
+		});
+	});
+</script>
+</html>
